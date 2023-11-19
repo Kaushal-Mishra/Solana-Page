b9c7a5e5d2c65af7c6f98114c3d6ff78e433dae6
Cosmic-Eye <72144900+Kaushal-Mishra@users.noreply.github.com>
2023-11-19 12:02:16 +0530

    Initial commit

---
+++ src/screens/Index/Index.jsx
@@ -0,0 +1,318 @@
+import React from "react";
+import "./style.css";
+
+export const Index = () => {
+  return (
+    <div className="index">
+      <div className="div">
+        <div className="overlap">
+          <div className="ellipse" />
+          <div className="ellipse-2" />
+          <p className="featured-on">
+            <span className="text-wrapper">featured on...</span>
+          </p>
+          <div className="slotto">
+            <img
+              className="element"
+              alt="Element"
+              src="https://cdn.animaapp.com/projects/6559a8c90d3a48ab5d2e1766/releases/6559a97e38fd7dfd97a3a670/img/6f745837-ba37-4ae9-89b6-4833ad984697-1-201-a-1-1@2x.png"
+            />
+            <div className="rectangle-wrapper">
+              <img
+                className="rectangle"
+                alt="Rectangle"
+                src="https://cdn.animaapp.com/projects/6559a8c90d3a48ab5d2e1766/releases/6559a97e38fd7dfd97a3a670/img/rectangle-1-1@2x.png"
+              />
+            </div>
+          </div>
+          <div className="rectangle-2">
+            <div className="time-line">
+              <div className="overlap-group">
+                <img
+                  className="line"
+                  alt="Line"
+                  src="https://cdn.animaapp.com/projects/6559a8c90d3a48ab5d2e1766/releases/6559a97e38fd7dfd97a3a670/img/line-5-1@2x.png"
+                />
+                <div className="ellipse-3" />
+                <div className="ellipse-4" />
+                <div className="ellipse-5" />
+                <div className="ellipse-6" />
+                <div className="ellipse-7" />
+              </div>
+            </div>
+            <div className="frame">
+              <p className="sept">
+                <span className="span">SEPT 2023</span>
+              </p>
+              <div className="completion-of-BUX-wrapper">
+                <p className="completion-of-BUX">
+                  <span className="text-wrapper-2">Completion of BUX$DAO website</span>
+                </p>
+              </div>
+              <div className="div-wrapper">
+                <p className="span-wrapper">
+                  <span className="text-wrapper-2">Completion of BUX$DAO website</span>
+                </p>
+              </div>
+            </div>
+            <div className="roadmap-info">
+              <div className="info-bar">
+                <img
+                  className="vector"
+                  alt="Vector"
+                  src="https://cdn.animaapp.com/projects/6559a8c90d3a48ab5d2e1766/releases/6559a97e38fd7dfd97a3a670/img/vector-1@2x.png"
+                />
+                <p className="the-following">
+                  <span className="text-wrapper-2">
+                    The following roadmap is intended to act as a guideline for our members as to the direction in which
+                    the project is heading. It should be understood that plans maybe subject to change depending on
+                    market trends and new technologies.
+                  </span>
+                </p>
+              </div>
+            </div>
+            <p className="heading">
+              <span className="text-wrapper-3">ROADMAP</span>
+            </p>
+          </div>
+          <img
+            className="hero-bottom"
+            alt="Hero bottom"
+            src="https://cdn.animaapp.com/projects/6559a8c90d3a48ab5d2e1766/releases/6559a97e38fd7dfd97a3a670/img/hero-bottom-1@2x.png"
+          />
+          <div className="hero-right">
+            <div className="discord-button">
+              <div className="download-wrapper">
+                <img
+                  className="download"
+                  alt="Download"
+                  src="https://cdn.animaapp.com/projects/6559a8c90d3a48ab5d2e1766/releases/6559a97e38fd7dfd97a3a670/img/download-1-1-1@2x.png"
+                />
+              </div>
+            </div>
+            <div className="frame-2">
+              <div className="frame-3">
+                <div className="ellipse-8" />
+                <p className="paragraph">
+                  <span className="text-wrapper-4">All artwork is tradable on the Solana blockchain</span>
+                </p>
+              </div>
+              <div className="frame-4">
+                <div className="ellipse-8" />
+                <p className="paragraph">
+                  <span className="text-wrapper-4">Daily staking rewards paid in our $BUX token</span>
+                </p>
+              </div>
+              <div className="frame-5">
+                <div className="ellipse-8" />
+                <p className="paragraph">
+                  <span className="text-wrapper-4">CASHOUT from our community staking pool!</span>
+                </p>
+              </div>
+              <div className="frame-6">
+                <div className="ellipse-8" />
+                <p className="paragraph">
+                  <span className="text-wrapper-4">Unlock holder only discord channels</span>
+                </p>
+              </div>
+            </div>
+            <p className="p">
+              <span className="text-wrapper-5">
+                Community owned
+                <br /> NFT art studio
+              </span>
+            </p>
+          </div>
+          <div className="hero-left">
+            <img
+              className="image"
+              alt="Image"
+              src="https://cdn.animaapp.com/projects/6559a8c90d3a48ab5d2e1766/releases/6559a97e38fd7dfd97a3a670/img/image0-1-1@2x.png"
+            />
+          </div>
+          <div className="TOKENOMICS">
+            <div className="overlap-2">
+              <img
+                className="polygon"
+                alt="Polygon"
+                src="https://cdn.animaapp.com/projects/6559a8c90d3a48ab5d2e1766/releases/6559a97e38fd7dfd97a3a670/img/polygon-4-1@2x.png"
+              />
+              <img
+                className="polygon"
+                alt="Polygon"
+                src="https://cdn.animaapp.com/projects/6559a8c90d3a48ab5d2e1766/releases/6559a97e38fd7dfd97a3a670/img/polygon-3-1@2x.png"
+              />
+              <div className="heading-wrapper">
+                <p className="heading-2">
+                  <span className="text-wrapper-3">TOKENOMICS</span>
+                </p>
+              </div>
+              <div className="the-BUX-token-can-be-wrapper">
+                <p className="the-BUX-token-can-be">
+                  <span className="text-wrapper-6">The $BUX token can be earned through any of the following...</span>
+                </p>
+              </div>
+              <div className="frame-7">
+                <div className="frame-8">
+                  <img
+                    className="img"
+                    alt="Vector"
+                    src="https://cdn.animaapp.com/projects/6559a8c90d3a48ab5d2e1766/releases/6559a97e38fd7dfd97a3a670/img/vector-2@2x.png"
+                  />
+                  <p className="span-wrapper-2">
+                    <span className="text-wrapper-2">Staking NFTs from any BUX$DAO collection</span>
+                  </p>
+                </div>
+                <div className="frame-9">
+                  <img
+                    className="vector-2"
+                    alt="Vector"
+                    src="https://cdn.animaapp.com/projects/6559a8c90d3a48ab5d2e1766/releases/6559a97e38fd7dfd97a3a670/img/vector-2@2x.png"
+                  />
+                  <p className="community-engagement">
+                    <span className="text-wrapper-2">Community engagement through our Discord and Twitter</span>
+                  </p>
+                </div>
+                <div className="frame-10">
+                  <img
+                    className="img"
+                    alt="Vector"
+                    src="https://cdn.animaapp.com/projects/6559a8c90d3a48ab5d2e1766/releases/6559a97e38fd7dfd97a3a670/img/vector-2@2x.png"
+                  />
+                  <p className="span-wrapper-2">
+                    <span className="text-wrapper-2">Poker tournaments, collaborations&nbsp;&nbsp;and giveaways</span>
+                  </p>
+                </div>
+              </div>
+              <div className="frame-11">
+                <div className="frame-12">
+                  <div className="group">
+                    <div className="overlap-group-2">
+                      <img
+                        className="vector-3"
+                        alt="Vector"
+                        src="https://cdn.animaapp.com/projects/6559a8c90d3a48ab5d2e1766/releases/6559a97e38fd7dfd97a3a670/img/vector-5@2x.png"
+                      />
+                      <div className="rectangle-3" />
+                    </div>
+                  </div>
+                  <p className="BUX-cannot-be-bought">
+                    <span className="text-wrapper-2">$BUX cannot be bought or sold on exchanges</span>
+                  </p>
+                </div>
+                <div className="frame-13">
+                  <div className="vector-wrapper">
+                    <img
+                      className="vector-3"
+                      alt="Vector"
+                      src="https://cdn.animaapp.com/projects/6559a8c90d3a48ab5d2e1766/releases/6559a97e38fd7dfd97a3a670/img/vector-6@2x.png"
+                    />
+                  </div>
+                  <div className="frame-14">
+                    <p className="BUX-can-be-cashed">
+                      <span className="text-wrapper-2">
+                        $BUX can be cashed out for Solana from our community staking wallet
+                      </span>
+                    </p>
+                    <p className="please-open-a">
+                      <span className="text-wrapper-7">
+                        (Please open a support ticket in our discord server for more details)
+                      </span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <img
+                className="solana-logo"
+                alt="Solana logo"
+                src="https://cdn.animaapp.com/projects/6559a8c90d3a48ab5d2e1766/releases/6559a97e38fd7dfd97a3a670/img/solana-logo-1@2x.png"
+              />
+              <div className="BUX-logo-wrapper">
+                <img
+                  className="BUX-logo"
+                  alt="Bux logo"
+                  src="https://cdn.animaapp.com/projects/6559a8c90d3a48ab5d2e1766/releases/6559a97e38fd7dfd97a3a670/img/bux-logo-1@2x.png"
+                />
+              </div>
+            </div>
+          </div>
+          <header className="header">
+            <div className="header-left">
+              <div className="navbar">
+                <p className="HOME">
+                  <span className="text-wrapper-8">HOME</span>
+                </p>
+                <p className="TOKENOMICS-2">
+                  <span className="text-wrapper-8">TOKENOMICS</span>
+                </p>
+                <p className="SLOTTO">
+                  <span className="text-wrapper-8">SLOTTO</span>
+                </p>
+                <p className="ROADMAP">
+                  <span className="text-wrapper-8">ROADMAP</span>
+                </p>
+                <p className="TEAM">
+                  <span className="text-wrapper-8">TEAM</span>
+                </p>
+              </div>
+            </div>
+            <div className="logo">
+              <div className="overlap-group-3">
+                <p className="BUX">
+                  <span className="text-wrapper-9">BUX</span>
+                </p>
+                <p className="span-wrapper-3">
+                  <span className="text-wrapper-10">$</span>
+                </p>
+                <p className="DAO">
+                  <span className="text-wrapper-9">DAO</span>
+                </p>
+                <div className="ellipse-9" />
+              </div>
+            </div>
+          </header>
+        </div>
+        <div className="side-menu">
+          <div className="apple-UI">
+            <div className="frame-15">
+              <div className="ellipse-10" />
+              <div className="ellipse-11" />
+              <div className="ellipse-12" />
+            </div>
+          </div>
+          <div className="profile">
+            <img
+              className="ham"
+              alt="Ham"
+              src="https://cdn.animaapp.com/projects/6559a8c90d3a48ab5d2e1766/releases/6559a97e38fd7dfd97a3a670/img/ham-1@2x.png"
+            />
+          </div>
+          <div className="menus">
+            <div className="menu">
+              <div className="button-template">
+                <div className="frame-16" />
+              </div>
+            </div>
+            <div className="menu-2">
+              <div className="frame-wrapper">
+                <div className="frame-17" />
+              </div>
+              <div className="button-template-2">
+                <div className="frame-18" />
+              </div>
+              <div className="button-template-3">
+                <div className="frame-19" />
+              </div>
+              <div className="button-template-4">
+                <div className="frame-20" />
+              </div>
+              <div className="button-template-5">
+                <div className="frame-21" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
